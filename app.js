c2828e6fd1b246f2693de979a723d1d36609c9c0
Skyler <skyler.sy.k@gmail.com>
2023-04-11 20:01:00 +0900

    Add server validation(feat. Joi tool)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
@@ -49,7 +50,18 @@ app.get('/campgrounds/new', (req, res)=>{
 app.post('/campgrounds',  catchAsync(async(req, res, next)=>{
     //res.send(req.body);
     // req.body는 {"campground":{"title":"오두막","location":"부산, 대한민국"}}
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
