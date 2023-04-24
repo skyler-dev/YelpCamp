e544be498e3a3358df323f275df15ea482560d33
Skyler <skyler.sy.k@gmail.com>
2023-04-24 21:54:41 +0900

    Fix register Route

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,17 @@ router.get('/register', (req, res) => {
 
 //디폴트 오류핸들러는 오류페이지를 보여주기 때문에 나쁜 사용자 경험 선사.
 //대신 flash 사용.
-router.post('/register', catchAsync(async (req, res)=>{
+router.post('/register', catchAsync(async (req, res, next)=>{
     try{
-    const {email, username, password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    req.flash('success','Welcome to Yelp Camp!');
-    res.redirect('/campgrounds');
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        //register하면 저절로 로그인 상태가 되게 하기
+        req.login(registeredUser, function(err) {
+            if (err) { return next(err); }
+            req.flash('success','Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('register');
