50f1c995018a026e45a9a6b67da1614d2298d4cb
Skyler <skyler.sy.k@gmail.com>
2023-04-26 20:08:24 +0900

    Refactor routes

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const passport = require('passport');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true}), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true}), users.login);
 
 //공격방지를 위해 연습 이후 POST라우트로 수정할 것.
 router.get('/logout', users.logout)
