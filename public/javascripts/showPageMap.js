5d8a4e3c910a8529edabae0252f7d4aca7b97c85
Skyler <skyler.sy.k@gmail.com>
2023-05-05 22:23:25 +0900

    add basic pin to map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,6 +3,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+// Create a new marker.
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates) //[lng, lat]
+    .addTo(map);
