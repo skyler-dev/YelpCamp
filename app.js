2ed2956363bfcb4ace2cd891ef770b7dbd4ebef9
Skyler <skyler.sy.k@gmail.com>
2023-06-01 13:46:01 +0900

    add basic security stuff

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const reviewRoutes = require('./routes/reviews');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => {
@@ -40,18 +43,75 @@ app.use(express.urlencoded({ extended: true }));
 //쿼리문자열로 사용할 문자열 전달. 변경 가능.
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+// $ 나 .가 포함된 키 허용하지 않기 위함. _로 대체함.
+app.use(
+    mongoSanitize({
+      replaceWith: '_',
+    }),
+  );
 
 const sessionConfig = {
+    // 기본값인 'connect.sid'대신 다른 이름 사용
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie:{
         maxAge: 1000*60*60*24*7,
+        // 스크립트삽입해서 쿠키접근하는 것 방지
         httpOnly: true,
+        // localhost는 HTTPS가 아님
+        // secure: true,
     }
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+// 리소스 불러오는 위치 제한
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/doo4mczu4/", //SHOULD MATCH CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
--- schemas.js
+++ schemas.js
@@ -1,19 +1,45 @@
-const Joi = require('joi');
 //mongoose 스카마가 아니고, Joi메서드를 사용해 정의한, JS객체를 위한 패턴이다.
+const BaseJoi = require('joi');
+
+// input으로 들어오는 HTML 태그 삭제
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+// extension의 escapeHTML으로 옵션 적용하기 위함
+const Joi = BaseJoi.extend(extension);
+
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 })
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(1).max(5)
     }).required()
 })
