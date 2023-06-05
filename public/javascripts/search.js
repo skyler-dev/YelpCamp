475dbe2e2099871de2bc5160e093ec5589d64743
Skyler <skyler.sy.k@gmail.com>
2023-06-05 17:36:26 +0900

    add searchBar

---
+++ public/javascripts/search.js
@@ -0,0 +1,24 @@
+
+const searchInput = document.querySelector("[data-search]");
+const cards = document.querySelectorAll(".card");
+const mapDiv = document.querySelector("#cluster-map");
+
+searchInput.addEventListener("focusin", e =>{
+    mapDiv.classList.add("hide");
+})
+
+searchInput.addEventListener("focusout", e =>{
+    if(!e.target.value){
+        mapDiv.classList.remove("hide");
+    }
+})
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase();
+    cards.forEach((card)=>{
+        const title = card.querySelector(".card-title");
+        const location = title.nextElementSibling.nextElementSibling;
+        const isVisible = title.textContent.toLowerCase().includes(value)||location.textContent.toLowerCase().includes(value);
+        card.classList.toggle("hide", !isVisible);
+    })
+  })
